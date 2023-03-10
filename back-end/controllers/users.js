71547eaa0b4feabb111937aa888f3b15ddcd435e
andyboli <webdevelopmentbolivar@gmail.com>
2023-03-10 18:26:35 -0300

    add files from bolibrary (private repository)

---
+++ back-end/controllers/users.js
@@ -0,0 +1,69 @@
+const Boom = require("@hapi/boom");
+
+const { users } = require("../services");
+
+const handleError = {
+  existUser: () => {
+    throw Boom.badRequest("Email already registered");
+  },
+  userNotFound: () => {
+    throw Boom.badRequest("User not exists");
+  },
+  wrongPassowrd: () => {
+    throw Boom.badRequest("Wrong Password");
+  },
+};
+
+const register = async (req, res) => {
+  const { error } = await users.register(req.body);
+
+  if (error) {
+    handleError[error]();
+  }
+
+  const { token } = await users.login(req.body);
+
+  res.status(201).json({ token, message: "User created with success!" });
+};
+
+const login = async (req, res) => {
+  const { error, token, user } = await users.login(req.body);
+
+  if (error) {
+    handleError[error]();
+  }
+
+  res.status(200).json({ token, user });
+};
+
+const find = async (req, res) => {
+  const user = await users.find(req.user);
+
+  res.status(200).json(user);
+};
+
+const update = async (req, res) => {
+  const { error } = await users.update(req.body);
+
+  if (error) {
+    handleError[error]();
+  }
+
+  res.status(200).json({ message: "User update with sucess!" });
+};
+
+const validToken = (_req, res) => res.status(200).json();
+
+const isAdmin = (req, res) => {
+  const { role } = req.user;
+  res.status(200).json({ role });
+};
+
+module.exports = {
+  find,
+  login,
+  register,
+  update,
+  validToken,
+  isAdmin,
+};
